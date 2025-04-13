3114b49e6ade9f72c8afff4ba119cf72c74df64a
Shree Harsha <shsmysore@gmail.com>
2025-04-13 23:02:19 +0530

    Complete second React project.

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+export default function Main() {
+    return (
+        <main className="main-container">
+            <section className="main-header-container">
+                <h1 className="main-header-text">Fun facts about React</h1>
+            </section>
+            <section className="main-body-container">
+                <ul className="main-facts-ul">
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 200K stars on GitHub</li>
+                    <li>Is maintained by Meta</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </section>
+        </main>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="src/assets/react.svg" alt="React Logo"/>
+                <p>ReactFacts</p>
+            </nav>
+        </header>
+    )
+}
